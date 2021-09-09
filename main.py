d615c2e5865fe8f2a9e2ad59c9503bfb79712c33
Qianqiu Zhang <43324225+zqq66@users.noreply.github.com>
2021-09-09 21:30:31 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
-import webbrowser
 
-webbrowser.open('http://google.com/')
+
 print(requests.__version__)
+google = requests.get('http://google.com/')
+print(google.text)
+python_script = requests.get('https://raw.githubusercontent.com/zqq66/404_lab1/main/main.py')
+print(python_script.text)
